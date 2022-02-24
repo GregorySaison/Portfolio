35bf73a3061494c94b660aa902dca6e7e1f022c0
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-02-24 18:01:57 +0100

    feat: Create and set up the server and routes with controller for all pages of the project

--- app/router.js
+++ app/router.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const renderController = require('./controllers/renderController.js');
+
+router.get('/', renderController.homePage);
+router.get('/about', renderController.aboutPage);
+router.get('/projects', renderController.projectsPage);
+router.get('/contact', renderController.contactPage);
+
 module.exports = router;
