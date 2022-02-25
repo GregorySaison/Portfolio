d0e4228992eaf41446a3335e7de79a1c5d64688a
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-02-25 16:59:46 +0100

    refactor(css loading): Move to an array implementation of the css files

--- app/controllers/renderController.js
+++ app/controllers/renderController.js
@@ -1,38 +1,35 @@
 const renderController = {
     homePage: (__, res) => {
-        res.render('home', { 
-            home: true,
-            about: false,
-            projects: false,
-            contact: false 
-        });
+        res.render('home', { cssSheets: [
+            'normalize',
+            'styles',
+            'home',
+            'http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome'
+        ] });
     },
 
     aboutPage: (__, res) => {
-        res.render('about', { 
-            home: false,
-            about: true,
-            projects: false,
-            contact: false 
-        });
+        res.render('about', { cssSheets: [
+            'normalize',
+            'styles',
+            'about',
+        ] });
     },
 
     projectsPage: (__, res) => {
-        res.render('projects', { 
-            home: false,
-            about: false,
-            projects: true,
-            contact: false 
-        });
+        res.render('projects', { cssSheets: [
+            'normalize',
+            'styles',
+            'projects',
+        ] });
     },
 
     contactPage: (__, res) => {
-        res.render('contact', { 
-            home: false,
-            about: false,
-            projects: false,
-            contact: true 
-        });
+        res.render('contact', { cssSheets: [
+            'normalize.css',
+            'styles.css',
+            'contact.css',
+        ] });
     }
 }
 
